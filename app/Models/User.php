e2cf436cc0fbe96618521504c2083f1e15fe5796
ujez <ujezujez@gmail.com>
2022-03-24 07:47:37 +0100

    removed the registration and the email verification, we assume those are already inputed, this is entirely optional.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -31,6 +30,11 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function listings()
+    {
+        return $this->hasMany(Listing::class);
+    }
 
     /**
      * The attributes that should be cast to native types.
--- database/migrations/2022_03_23_141342_create_listing_tag_table.php
+++ database/migrations/2022_03_23_141342_create_listing_tag_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTagsTable extends Migration
+class CreateListingTagTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateTagsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tags', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('slug');
-            $table->timestamps();
+        Schema::create('listing_tag', function (Blueprint $table) {
+            $table->unsignedBigInteger('listing_id');
+            $table->unsignedBigInteger('tag_id');
         });
     }
 
@@ -28,6 +26,6 @@ class CreateTagsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tags');
+        Schema::dropIfExists('listing_tag');
     }
 }
