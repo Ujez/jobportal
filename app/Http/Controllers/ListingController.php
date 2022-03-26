0069fe607e0faed10488b2d1a0efd19e036493f4
ujez <ujezujez@gmail.com>
2022-03-26 09:56:31 +0100

    you can now filter by the name and search for any specific keyword

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -2,20 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
 use App\Models\Listing;
 use App\Models\Tag;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class ListingController extends Controller
 {
-    public function index(){
-        $listings = Listing::where('is_active', true)
-        ->with('tags')
-        ->latest()
-        ->get();
+    public function index(Request $request)
+    {
+        $query = Listing::query()
+            ->where('is_active', true)
+            ->with('tags')
+            ->latest();
+
+        if ($request->has('s')) {
+            $searchQuery = trim($request->get('s'));
+
+            $query->where(function (Builder $builder) use ($searchQuery) {
+                $builder
+                    ->orWhere('title', 'like', "%{$searchQuery}%")
+                    ->orWhere('company', 'like', "%{$searchQuery}%")
+                    ->orWhere('location', 'like', "%{$searchQuery}%");
+            });
+        }
+
+        if ($request->has('tag')) {
+            $tag = $request->get('tag');
+            $query->whereHas('tags', function (Builder $builder) use ($tag) {
+                $builder->where('slug', $tag);
+            });
+        }
+
+        $listings = $query->get();
 
         $tags = Tag::orderBy('name')
-        ->get();
+            ->get();
 
         return view('listings.index', compact('listings', 'tags'));
     }
