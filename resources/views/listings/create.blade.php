5e60a9743e078a0c04706a9ea0acab800f0d8430
ujez <ujezujez@gmail.com>
2022-07-16 17:54:47 +0100

    little update

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -86,7 +86,7 @@
                     @csrf
                     <input type="hidden" id="payment_method_id" name="payment_method_id" value="">
                     <button type="submit" id="form_submit"
-                        class="hover:animate-spin block w-full items-center bg-indigo-500 text-white border-0 py-2 focus:outline-none hover:bg-indigo-600 rounded text-base mt-4 md:mt-0">Pay
+                        class="block w-full items-center bg-indigo-500 text-white border-0 py-2 focus:outline-none hover:bg-indigo-600 rounded text-base mt-4 md:mt-0">Pay
                         + Continue</button>
                 </div>
             </form>
