0e16fdff982c98f68895e82997151edcf692c4df
ujez <ujezujez@gmail.com>
2022-08-04 21:51:36 +0100

    little update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,7 @@
                     to your newsletter to stay in the loop. Our newsletter is sent once in
                     a week on every friday so subscribe to get latest news and updates.
                 <form method="post" action="{{ url('newsletter/store') }}">
-                    {{-- @csrf --}}
+                    @csrf
                     <div class="flex items-stretch mt-12">
                         <input name="email"
                             class="bg-gray-100 rounded-lg rounded-r-none dark:bg-gray-800 text-base
